e57cdb342dd0baea629788272ddb318d5c32a6c0
Karera-o <olivierkarera2020@gmail.com>
2025-04-17 23:41:57 +0200

    Fixed padding issue

--- src/components/sections/WasteValueSection.tsx
+++ src/components/sections/WasteValueSection.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 
-
 const WasteValueSection = () => {
   return (
     <section className="bg-[#1E2A36] text-white py-10 rounded-4xl">
-      <div className="container mx-auto">
+      <div className="container mx-auto px-6 md:px-10 max-w-[90%]">
         <h2 className="header-h1">Turning Waste Into Value</h2>
         <h3 className="header-h2">
-        Optimize your waste management with us—from <br/> Circular Waste Scans to Green Sourcing.
+          Optimize your waste management with us—from <br className="hidden md:block"/>
+          Circular Waste Scans to Green Sourcing.
         </h3>
         <p className="text-p1 mx-auto">
-          
-          Our market place will facilitate the exchange of surplus materials between <br/> businesses.
-          Help you cut costs, enhance resource efficiency, boosting <br/>  profits, reducing environmental
-          impact and ensure full regulatory compliance.<br/>  Transform sustainability goals into
-          measurable success with our innovative<br/>  circular approach.
+          Our market place will facilitate the exchange of surplus materials between
+          <br className="hidden md:block"/> businesses.
+          Help you cut costs, enhance resource efficiency, boosting
+          <br className="hidden md:block"/> profits, reducing environmental
+          impact and ensure full regulatory compliance.
+          <br className="hidden md:block"/> Transform sustainability goals into
+          measurable success with our innovative
+          <br className="hidden md:block"/> circular approach.
         </p>
-        
       </div>
     </section>
   );
