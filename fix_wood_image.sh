f654aa5abaa11170e75a0b3c4b0f019cd8468672
Karera-o <olivierkarera2020@gmail.com>
2025-04-28 22:32:00 +0200

    wip

---
+++ fix_wood_image.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Fix the wood-alt image
+curl -o public/images/marketplace/categories/wood-alt.jpg https://images.unsplash.com/photo-1611023496268-8d6c4e6d2b74
+
+echo "Wood image fixed!"
