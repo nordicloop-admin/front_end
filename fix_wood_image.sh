15dd26c4203e9822c9ff479bb69091ec81965163
Olivier KARERA <82539826+Karera-o@users.noreply.github.com>
2025-04-30 10:49:04 +0200

    Removing un needed files

--- fix_wood_image.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Fix the wood-alt image
-curl -o public/images/marketplace/categories/wood-alt.jpg https://images.unsplash.com/photo-1611023496268-8d6c4e6d2b74
-
-echo "Wood image fixed!"
