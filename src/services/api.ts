c9693eb59fbc20778104c990021886fb5238b340
Karera-o <olivierkarera2020@gmail.com>
2025-06-06 19:23:29 +0200

    added right url

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,7 +5,6 @@ import { getAccessToken } from '@/services/auth';
 
 // Base URL for the API
 const API_BASE_URL = 'https://nordic-loop-platform.onrender.com/api';
-// const API_BASE_URL = 'http://127.0.0.1:8000/api';
 
 /**
  * Interface for API response
